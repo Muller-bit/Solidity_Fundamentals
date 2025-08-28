d3c7dc63156fd8ab0d89650498db08aceb5d8669
Mulualem <mulualemdarimo76@gmail.com>
2025-08-28 03:35:03 +0200

    first commit

--- solidity.sol
+++ solidity.sol
@@ -1,2 +1,5 @@
-//SPDX-License-identifier: MIT
+//SPDX-License-identifier: MIT //this specifies how others can use your code with an SPDX license identifier
 pragma solidity ^0.8.19;
+contract MyContract {
+    
+}
