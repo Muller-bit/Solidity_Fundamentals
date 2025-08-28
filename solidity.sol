7920705a7486d8aee0df73435e88ac009c023992
Mulualem <mulualemdarimo76@gmail.com>
2025-08-28 03:40:16 +0200

    first commit

--- solidity.sol
+++ solidity.sol
@@ -1,5 +1,13 @@
 //SPDX-License-identifier: MIT //this specifies how others can use your code with an SPDX license identifier
 pragma solidity ^0.8.19;
 contract MyContract {
-    
+    // Contract code goes here
+}
+//Variables: Storing Information
+contract StorageExample {
+    uint256 public myNumber = 42; // A number
+    string public myText = "Hello"; // Text
+    bool public isActive = true; // A true/false value
+    address public owner; // An Ethereum address
+    uint256 private secretNumber; // A private number
 }
