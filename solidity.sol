2bbcfa336e5b15f7e8e6863742b971b36407ce7c
Mulualem <mulualemdarimo76@gmail.com>
2025-09-03 21:56:36 +0200

    update files - Solidity fundamentals

--- solidity.sol
+++ solidity.sol
@@ -150,5 +150,7 @@ contract Counter {
         count = count + 1; // You can also write: count += 1;
     }
     // This function decreases the count by 1
-    function decrement() public {}
+    function decrement() public {
+        count = count - 1; // You can also write: count -= 1;
+    }
 }
