51c2a79c20af2e170e75a7d946e2cd28972be392
Mulualem <mulualemdarimo76@gmail.com>
2025-09-05 03:02:48 +0200

    update files - Solidity fundamentals

--- solidity.sol
+++ solidity.sol
@@ -181,4 +181,8 @@ contract child is parent {
     function addNumbers(uint256 a, uint256 b) public pure returns (uint256) {
         return a + b;
     }
+    //constructor: Runs only once when the contract is deployed
+    constructor() {
+        owner = msg.sender; // Sets the contract creator as the owner
+    }
 }
