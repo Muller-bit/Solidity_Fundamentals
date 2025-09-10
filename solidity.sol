200f6ed1aacf79354b9f1603c6351e4d6ed1e599
Mulualem <mulualemdarimo76@gmail.com>
2025-09-10 14:30:43 +0200

    update files - Solidity fundamentals

--- solidity.sol
+++ solidity.sol
@@ -331,5 +331,6 @@ contract TimeLockedWallet {
 
     function sumArray(uint256[] memory numbers) public pure returns (uint256) {
         uint256 total = 0;
+        for (uint256 i = 0; i < numners.length; i++) {}
     }
 }
