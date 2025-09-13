849acc2238aff857d5c5549204c6f169f9a6db80
Mulualem <mulualemdarimo76@gmail.com>
2025-09-13 16:04:14 +0200

    update files - Solidity fundamentals

--- solidity.sol
+++ solidity.sol
@@ -418,4 +418,24 @@ interface IPayable {
     function pay(address recipient, uint256 amount) external returns (bool);
     function getBalance(address account) external view returns (uint256);
 }
+
 //Any contract that implements this interface must include these exact functions:
+contract PaymentProcessor is IPayable {
+    mapping(address => uint256) private balances;
+
+    function pay(
+        address recipient,
+        uint256 amount
+    ) external override returns (bool) {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+        return true;
+    }
+
+    function getBalance(
+        address account
+    ) external view override returns (uint256) {
+        return balances[account];
+    }
+}
