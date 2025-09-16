1e765b1572ca00c280c168863acf26805367878b
Mulualem <mulualemdarimo76@gmail.com>
2025-09-16 17:40:42 +0200

    update files - Solidity fundamentals

--- solidity.sol
+++ solidity.sol
@@ -462,4 +462,5 @@ contract SimpleMath {
 
     //ABI-tells you that there is a function named add , which takes two numbers (of type uint256) and returns a number (also of type uint256)
     //Frontend applications use the ABI to format calls to your contract correctly.
+    //mostly use the use the ABI to interact with a smart contract (using the ethers.js library):
 }
