bef0b03a71b5ad1f8b97e48a48fc6b4cf3329b9c
Mulualem <mulualemdarimo76@gmail.com>
2025-09-19 07:00:11 +0200

    Add contract_library.sol

--- contract_library.sol
+++ contract_library.sol
@@ -1,3 +1,4 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 // This line specifies the version of Solidity to be used
+//Library and inheritence are two important concepts in Solidity that help in code reusability and organization
