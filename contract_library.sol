94489f7e5e3bd8b1642179bb9b6b28973b8ff433
Mulualem <mulualemdarimo76@gmail.com>
2025-09-19 09:15:30 +0200

    Add contract_library.sol

--- contract_library.sol
+++ contract_library.sol
@@ -3,3 +3,4 @@ pragma solidity ^0.8.30;
 // This line specifies the version of Solidity to be used
 //Library and inheritence are two important concepts in Solidity that help in code reusability and organization
 //Contain hlful functions that can be used by other contracts
+library MathUtils {}
