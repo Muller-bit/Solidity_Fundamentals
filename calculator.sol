c5e7c8ad5c5b6e5d8018235564c2e64a13e1286d
Mulualem <mulualemdarimo76@gmail.com>
2025-09-20 12:16:00 +0200

    Add contractl

--- calculator.sol
+++ calculator.sol
@@ -1,3 +1,4 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19; // make sure the version matches the one in MathUtils.sol
 import "./MathUtils.sol"; // Import the library
+contract Calculator {}
