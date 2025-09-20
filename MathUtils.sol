eda28593703e218b73c9cabeb817601633f1a83d
Mulualem <mulualemdarimo76@gmail.com>
2025-09-20 12:10:55 +0200

    Add contractl

--- MathUtils.sol
+++ MathUtils.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.19;
 // This line specifies the version of Solidity to be used
 //Library and inheritence are two important concepts in Solidity that help in code reusability and organization
 //Contain hlful functions that can be used by other contracts
---
+++ calculator.sol
@@ -0,0 +1,3 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19; // make sure the version matches the one in MathUtils.sol
+import "./MathUtils.sol"; // Import the library
