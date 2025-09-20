2e40e2eb845580a80948ed7e97f6cec683871ea3
Mulualem <mulualemdarimo76@gmail.com>
2025-09-20 12:17:38 +0200

    Add contractl

--- calculator.sol
+++ calculator.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.19; // make sure the version matches the one in MathUtils.s
 import "./MathUtils.sol"; // Import the library
 contract Calculator {
     //attach the library to uint256 type
+    using MathUtils for uint256;
 }
